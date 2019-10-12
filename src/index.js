6c991765bd36167b436bb3ac160fbaa5bf633147
Viktar Dzmitryieu <vitaminnus@gmail.com>
2019-10-12 13:10:28 +0300

    fix/ update logging

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const http = require('http');
+
 const port = 4000;
+
 const server = http.createServer((req, res) => {
   if (req.url === '/') {
     console.log('start');
@@ -8,11 +10,11 @@ const server = http.createServer((req, res) => {
     }, Number(process.env.INTERVAL)*1000);
     setTimeout(() => {
       clearInterval(timeout);
-      console.log('stop');
+      console.log('stop timeout');
       res.end(new Date().toUTCString());
     }, Number(process.env.TIME)*1000)
   }
 });
 server.listen(port, () => {
-  console.log('server run!')
+  console.log(`server on port ${port} run!`)
 })
